8656bdfad1fece7acd094aceeaa36764eaf67eda
wangwenkai660 <1039370389@qq.com>
2019-08-07 10:07:20 +0800

    111

--- day01/src/com/itheima/test/Singleton.java
+++ day01/src/com/itheima/test/Singleton.java
@@ -7,6 +7,7 @@ public class Singleton {
         System.out.println("王文凯的单例模式");
         System.out.println(111);
         System.out.println("22222222222222");
+        System.out.println("222");
     }
 
 }
