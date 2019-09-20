ca9287f2753555b21655540bcf61b4352b617798
wangwenkai660 <1039370389@qq.com>
2019-09-20 20:55:25 +0800

    王文凯的修改

--- day01/src/com/itheima/test/Singleton.java
+++ day01/src/com/itheima/test/Singleton.java
@@ -10,10 +10,8 @@ public class Singleton {
         System.out.println("222");
         System.out.println("888");
         System.out.println("999");
-
-
-
         System.out.println("998");
+        System.out.println("10000");
 
     }
 
